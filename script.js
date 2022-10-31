8439cdba0b3caf4a3e4b02e095f0dff9539051b9
Jasmine Wongphatarakul <110138906+jjasminew@users.noreply.github.com>
2022-10-31 06:20:32 +0000

    Added scenarios and audio.

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 window.onload=function(){
   document.getElementByID("audio1").play();
-  document.getElementByID("audio2").play();
 }
+
+setTimeout(function(){
+      document.getElementById("audio2").play(); 
+    }, 8000)
+
+setTimeout(function(){
+      document.getElementById("audio3").play(); 
+    }, 13000)
+
+setTimeout(function(){
+      document.getElementById("audio4").play(); 
+    }, 10000)
+
+setTimeout(function(){
+      document.getElementById("audio5").play(); 
+    }, 19000)
+
+setTimeout(function(){
+      document.getElementById("audio6").play(); 
+    }, 17000)
+
+setTimeout(function(){
+      document.getElementById("audio7").play(); 
+    }, 6000)
+
 
 
 
