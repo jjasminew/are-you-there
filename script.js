6f8b3d2f62349304ed641fdf61f246af53e217ba
Jasmine Wongphatarakul <110138906+jjasminew@users.noreply.github.com>
2022-11-19 08:29:32 +0000

    Edited audio and combined audios

--- script.js
+++ script.js
@@ -2,31 +2,6 @@ window.onload=function(){
   document.getElementByID("audio1").play();
 }
 
-setTimeout(function(){
-      document.getElementById("audio2").play(); 
-    }, 8000)
-
-setTimeout(function(){
-      document.getElementById("audio3").play(); 
-    }, 13000)
-
-setTimeout(function(){
-      document.getElementById("audio4").play(); 
-    }, 10000)
-
-setTimeout(function(){
-      document.getElementById("audio5").play(); 
-    }, 19000)
-
-setTimeout(function(){
-      document.getElementById("audio6").play(); 
-    }, 17000)
-
-setTimeout(function(){
-      document.getElementById("audio7").play(); 
-    }, 6000)
-
-
 
 
 
